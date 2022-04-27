f22905c24bb48a4bfd530dc18f5834d0cd4ce7cb
Anurag Kumar <2002anuragksingh@gmail.com>
2022-04-27 15:07:00 +0530

    deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-423_q!ed*!#ia$=qlin)p119=s6!q7+j#b-u14j^nr6qqc=hiu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.whiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.static import static
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('data/', include('api.urls')),
+    url(r'^media/(PP<path>_*)$', serve, {"document_root": settings.MEDIA_ROOT}), 
+    url(r'instatic/(?P<path>.")$',serve, {"document_root": settings.STATIC_ROOT}):
+
+   settings.STATIC_ROOT),
 ]
